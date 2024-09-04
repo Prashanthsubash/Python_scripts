a203b6dc3b75a1958078885f50c404fb4a9e1c35
Prashanthsubash <Prashanthsubash29@gmail.com>
2024-09-04 14:16:11 +0530

    Add files via upload

---
+++ File_handling.py
@@ -0,0 +1,22 @@
+# file_path = (r"C:\Users\prashanth.subash\Downloads\Training-Plan-Aug-2024.xlsx")
+# with open (file_path,"rb") as file:
+#     content =  file.read()
+
+# print(content)
+
+# file_w = open("D:\Python_Training\demofile.txt","a")
+# print(file_w.write("oops i forgot something"))
+# file_w.close
+
+# file = open("D:\Python_Training\demofile.txt","r")
+# print(file.read())
+
+
+
+# file_test_w = open("D:\Python_Training\demofile.txt","a")
+# print(file_test_w.write(" sample content"))
+# file_test_w.close
+
+
+
+
